8fc3fe2603a6d21a0bcc45bd354d47c17ad39a0b
kuba-04 <jakub.gladysz1@gmail.com>
2024-11-05 14:29:55 +0100

    add keyboard actions

---
+++ README.md
@@ -0,0 +1,4 @@
+## Effort to build a game by learning rusty engine
+
+add collider lib to create collider files for images:
+`cargo install rusty_engine --example collider`
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,19 @@ use rusty_engine::prelude::*;
 #[derive(Resource)]
 struct GameState {
     high_score: u32,
-    current_score: u32,
-    enemy_labels: Vec<String>,
-    spawn_timer: Timer,
+    score: u32,
+    target_index: i32,
+    // enemy_labels: Vec<String>,
+    // spawn_timer: Timer,
 }
 
 impl Default for GameState {
     fn default() -> Self {
         Self {
             high_score: 0,
-            current_score: 0,
-            enemy_labels: Vec::new(),
-            spawn_timer: Timer::from_seconds(1.0, TimerMode::Once),
+            score: 0,
+            target_index: 0,
+            // spawn_timer: Timer::from_seconds(1.0, TimerMode::Once),
         }
     }
 }
@@ -27,15 +28,18 @@ fn main() {
     player.scale = 1.0;
     player.collision = true;
 
-    let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
-    car1.translation = Vec2::new(300.0, 0.0);
-    car1.collision = true;
+    let score = game.add_text("score", "Score: 0");
+    score.translation = Vec2::new(520.0, 320.0);
+
+    let high_score = game.add_text("high_score", "High Score: 0");
+    high_score.translation = Vec2::new(-520.0, 320.0);
 
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    // handle collisions
     for event in engine.collision_events.drain(..) {
         if event.state == CollisionState::Begin && event.pair.one_starts_with("player") {
             // remove the sprite the player collided with
@@ -45,10 +49,49 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
                 }
             }
         }
-        game_state.current_score += 1;
-        println!("Current score: {}", game_state.current_score);
+        game_state.score += 1;
+        let score = engine.texts.get_mut("score").unwrap();
+        score.value = format!("Score: {}", game_state.score);
+        if game_state.score > game_state.high_score {
+            game_state.high_score = game_state.score;
+            let high_score = engine.texts.get_mut("high_score").unwrap();
+            high_score.value = format!("High score: {}", game_state.high_score);
+        }
+
+        println!("Current score: {}", game_state.score);
     }
 
+    // handle movement
     let player = engine.sprites.get_mut("player").unwrap();
-    player.translation.x += 100.0 * engine.delta_f32;
+    const MOVEMENT_SPEED: f32 = 200.0;
+
+    if engine.keyboard_state.pressed(KeyCode::Up) {
+        player.translation.y += MOVEMENT_SPEED * engine.delta_f32;
+    }
+    if engine.keyboard_state.pressed(KeyCode::Down) {
+        player.translation.y -= MOVEMENT_SPEED * engine.delta_f32;
+    }
+    if engine.keyboard_state.pressed(KeyCode::Right) {
+        player.translation.x += MOVEMENT_SPEED * engine.delta_f32;
+    }
+    if engine.keyboard_state.pressed(KeyCode::Left) {
+        player.translation.x -= MOVEMENT_SPEED * engine.delta_f32;
+    }
+
+    if engine.mouse_state.just_pressed(MouseButton::Left) {
+        if let Some(mouse_location) = engine.mouse_state.location() {
+            let label = format!("zebra_{}", game_state.target_index);
+            game_state.target_index += 1;
+            let zebra = engine.add_sprite(label.clone(), "sprite/custom/zebra.png");
+            zebra.translation = mouse_location;
+            zebra.collision = true;
+            zebra.scale = 0.1;
+        }
+    }
+
+    if engine.keyboard_state.just_pressed(KeyCode::R) {
+        game_state.score = 0;
+        let score = engine.texts.get_mut("score").unwrap();
+        score.value = "Score: 0".to_string();
+    }
 }
